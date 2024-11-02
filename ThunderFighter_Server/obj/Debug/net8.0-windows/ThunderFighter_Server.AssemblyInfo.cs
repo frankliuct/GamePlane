818c0ee645b8e8903e61a150f12fe814894c3c3b
TSGFX <grcsxb269@163.com>
2024-11-02 22:37:00 +0800

    添加服务器

--- ThunderFighter_Server/obj/Debug/net8.0-windows/ThunderFighter_Server.AssemblyInfo.cs
+++ ThunderFighter_Server/obj/Debug/net8.0-windows/ThunderFighter_Server.AssemblyInfo.cs
@@ -11,12 +11,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("ThunderFighter")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("ThunderFighter_Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+87fc1c3b72f0b9d4ecc70742cad6ffff8a4379a8")]
-[assembly: System.Reflection.AssemblyProductAttribute("ThunderFighter")]
-[assembly: System.Reflection.AssemblyTitleAttribute("ThunderFighter")]
+[assembly: System.Reflection.AssemblyProductAttribute("ThunderFighter_Server")]
+[assembly: System.Reflection.AssemblyTitleAttribute("ThunderFighter_Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 [assembly: System.Runtime.Versioning.TargetPlatformAttribute("Windows7.0")]
 [assembly: System.Runtime.Versioning.SupportedOSPlatformAttribute("Windows7.0")]
